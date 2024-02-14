199c984ce84cf56d9e8ac9f3b6778b8083ae533e
melekladh <ladharimalek10@gmail.com>
2024-02-14 16:46:53 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 class App extends React.Component {
   // Create a field that shows the time interval since the last 
   // component was mounted using the component lifecycle.
-  componentDidMount() { 
-setInterval(()=>{this.setState({timer:this.state.timer+1});console.log(this.state.timer)}
-,1000)
-  }
+//   componentDidMount() { 
+// setInterval(()=>{this.setState({timer:this.state.timer+1});console.log(this.state.timer)}
+// ,1000)
+//   }
   //creating a state containing a Person  and a boolean shows and a timer
   state = {
     Person:{fullName:"malek",
@@ -43,8 +43,8 @@ console.log(this.state.shows)
     this.state.shows && <> <h2>{this.state.Person.fullName}</h2>
     <h2>{this.state.Person.bio}</h2></>
   }
-I added a button that toggles the show state.
-When the show state is true, the person's profile will appear.
+{/* I added a button that toggles the show state.
+When the show state is true, the person's profile will appear. */}
       <button onClick={()=>handleClick()}> ClickMe</button>
 <h1>{this.state.timer}</h1>
     </>
